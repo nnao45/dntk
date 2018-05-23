87036063f6bdbbc6907da60250eec49120a0ddca
nnao45 <n4sekai5y@gmail.com>
2018-05-23 19:03:33 +0900

    nikka

--- dntk.go
+++ dntk.go
@@ -28,8 +28,16 @@ func newline() *line {
 	}
 }
 
-//func bufferLine
-
+func (l *line) remove() {
+	var ary []byte = make([]byte, 32)
+	for i, b := range l.Buffer {
+		if i == len(l.Buffer)-1 {
+			break
+		}
+		ary = append(ary, b)
+	}
+	copy(ary, l.Buffer)
+}
 func ttyctl() {
 	// disable input buffering
 	exec.Command("stty", "-F", "/dev/tty", "cbreak", "min", "1").Run()
@@ -41,16 +49,11 @@ func ttyctl() {
 	defer exec.Command("stty", "-F", "/dev/tty", "echo").Run()
 }
 
-//func keyHook(devs []*keylogger.InputDevice) {
-//	for _, val := range devs {
-//		fmt.Println("Id->", val.Id, "Device->", val.Name)
-//	}
-//}
-
 func init() {
 	app.HelpFlag.Short('h')
 	app.Version(fmt.Sprint("dntk's version: ", version))
 	switch kingpin.MustParse(app.Parse(os.Args[1:])) {
+	//
 	}
 }
 
@@ -59,19 +62,14 @@ func main() {
 
 	l := newline()
 
-	//devs, err := keylogger.NewDevices()
-	//if err != nil {
-	//	panic(err)
-	//}
-
-	//go keyHook(devs)
-
 	for {
 		os.Stdin.Read(l.RuneByte)
 		l.Buffer = append(l.Buffer, l.RuneByte...)
-		//if string(b.RuneByte) == "" {
 
-		//}
+		if string(l.RuneByte) == "\b" {
+			l.remove()
+		}
+
 		fmt.Print("\r", string(l.Buffer))
 	}
 }

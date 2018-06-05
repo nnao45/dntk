e9a178756cb3b2a0f3ce6eb95ecedb814486b083
nnao45 <n4sekai5y@gmail.com>
2018-06-05 10:25:15 +0900

    funcmode make start

--- dntk.go
+++ dntk.go
@@ -52,21 +52,23 @@ func printCyan(s string) string {
 }
 
 const (
-	SIN_FUNCTION_KEY    = "[115]"
-	COS_FUNCTION_KEY    = "[99]"
-	ATAN_FUNCTION_KEY   = "[97]"
-	LOG_FUNCTION_KEY    = "[108]"
-	EXP_FUNCTION_KEY    = "[101]"
-	BESSEL_FUNCTION_KEY = "[106]"
+	SIN_FUNCTION_KEY      = "[115]" //s
+	COS_FUNCTION_KEY      = "[99]"  //c
+	ATAN_FUNCTION_KEY     = "[97]"  //a
+	LOG_FUNCTION_KEY      = "[108]" //l
+	EXP_FUNCTION_KEY      = "[101]" //e
+	BESSEL_FUNCTION_KEY   = "[106]" //j
+	RBRACKET_FUNCTION_KEY = "[40]"  //(
 )
 
 var funcMap map[string]string = map[string]string{
-	SIN_FUNCTION_KEY:    "SIN_FUNCTION_KEY",
-	COS_FUNCTION_KEY:    "COS_FUNCTION_KEY",
-	ATAN_FUNCTION_KEY:   "ATAN_FUNCTION_KEY",
-	LOG_FUNCTION_KEY:    "LOG_FUNCTION_KEY",
-	EXP_FUNCTION_KEY:    "EXP_FUNCTION_KEY",
-	BESSEL_FUNCTION_KEY: "BESSEL_FUNCTION_KEY",
+	SIN_FUNCTION_KEY:      "SIN_FUNCTION_KEY",
+	COS_FUNCTION_KEY:      "COS_FUNCTION_KEY",
+	ATAN_FUNCTION_KEY:     "ATAN_FUNCTION_KEY",
+	LOG_FUNCTION_KEY:      "LOG_FUNCTION_KEY",
+	EXP_FUNCTION_KEY:      "EXP_FUNCTION_KEY",
+	BESSEL_FUNCTION_KEY:   "BESSEL_FUNCTION_KEY",
+	RBRACKET_FUNCTION_KEY: "RBRACKET_FUNCTION_KEY",
 }
 
 type line struct {
@@ -74,6 +76,7 @@ type line struct {
 	Buffer         []byte
 	BufferAndEqual []byte
 	Flag           bool
+	FuncMode       bool
 }
 
 func newline() *line {
@@ -176,6 +179,10 @@ func main() {
 
 		addog(fmt.Sprintln(l.RuneByte), "./test.txt")
 
+		if l.FuncMode {
+			//TODO
+		}
+
 		if fmt.Sprint(l.RuneByte) == "[127]" {
 			// send delete key OR backspace key
 			l.Buffer = l.remove()
@@ -189,6 +196,12 @@ func main() {
 			break
 		} else if _, ok := funcMap[string(l.RuneByte)]; ok {
 			//TODO
+			if string(l.RuneByte) != "[40]" {
+				l.Buffer = append(l.Buffer, l.RuneByte...)
+			}
+			l.FuncMode = true
+			l.BufferAndEqual = append(append([]byte("(dntk): "), append(l.Buffer, []byte(" = ")...)...), result...)
+			continue
 		} else if string(l.RuneByte) == "[9]" {
 			// send tab key
 			// TODO

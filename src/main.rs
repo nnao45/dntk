7c87b49ab7beaa81cd603f6f9353dd139442a502
nnao45 <n4sekai5y@gmail.com>
2019-05-24 10:40:56 +0900

    impl defer & refact

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,12 @@
 #[macro_use]
 extern crate bc;
 extern crate libc;
-extern crate sdl2;
 
-use sdl2::keyboard::Keycode;
-use sdl2::sys;
-use std::convert::From;
-use std::error;
+#[macro_use(defer)] extern crate scopeguard;
+
 use std::error::Error;
-use std::ffi::CStr;
 use std::fmt;
 use std::io::Write;
-use std::time::SystemTime;
-use std::{thread, time};
 
 fn main() {
     let mut saved_termattr = libc::termios {
@@ -25,7 +19,7 @@ fn main() {
         c_ospeed: 0,
     };
     unsafe {
-        let mut ptr = &mut saved_termattr;
+        let ptr = &mut saved_termattr;
         libc::tcgetattr(0, ptr);
     }
     let mut termattr = saved_termattr;
@@ -38,6 +32,11 @@ fn main() {
     unsafe {
         libc::fcntl(0, libc::F_SETFL, libc::O_NONBLOCK);
     }
+    defer!(
+        unsafe {
+            libc::tcsetattr(0, libc::TCSANOW, &saved_termattr);
+        }
+    );
     let mut buf: [libc::c_char; 1] = [0; 1];
     let ptr = &mut buf;
 
@@ -46,28 +45,27 @@ fn main() {
     loop {
         let r = unsafe { libc::read(0, ptr.as_ptr() as *mut libc::c_void, 1) };
         if r > 0 {
-            let input_char = ptr[0] as u8 as char;
-            // match dntk_scan(input_char) {
-            //     None => (),
-            //     Some(c) => input_vec.push(c),
-            // }
-            input_vec.push(input_char);
-            print!("\r(dntk): ");
-            print!("{:?}", input_vec);
-            print!(" = ");
-            // print!(
-            //     "{}",
-            //     bc!(format!(
-            //         "{} + {} + {}",
-            //         input_vec[0], input_vec[0], input_vec[0]
-            //     ))
-            //     .unwrap()
-            // );
+            let input_char = ptr[0] as u8;
+            match char_scan(input_char) {
+                None => (),
+                Some(i) => {
+                    &input_vec.push(i);
+                    },
+            }
+            let p1 = "\r(dntk): ";
+            print!("{}", p1);
+            let p2 = std::str::from_utf8(&input_vec).unwrap_or(&"error occured");
+            print!("{}", p2);
+            let p3 = " = ";
+            print!("{}", p3);
+            match bc!(format!("{}", p2)) {
+                Ok(p4) => {
+                    print!("{}", &p4);
+                    },
+                _ => (),
+            }
         }
         std::io::stdout().flush().unwrap();
-    }
-    unsafe {
-        libc::tcsetattr(0, libc::TCSANOW, &saved_termattr);
     }
 }
 
@@ -95,23 +93,33 @@ impl Error for ScanError {
     }
 }
 
-fn dntk_scan(ascii_char: char) -> Option<char> {
+fn char_scan(ascii_char: u8) -> Option<u8> {
     match ascii_char {
-        's' => Some('s'),
-        'c' => Some('c'),
-        'a' => Some('a'),
-        'l' => Some('l'),
-        'e' => Some('e'),
-        'j' => Some('j'),
-        '(' => Some('('),
-        ')' => Some(')'),
-        '+' => Some('+'),
-        '-' => Some('-'),
-        '/' => Some('/'),
-        '*' => Some('*'),
-        '\n' => Some('\n'), // \n
-        '\u{1b}' => Some('\u{1b}'), // escape key
-        '\u{1b}' => Some('\u{7f}'), // delete key
+        48 => Some(48),
+        49 => Some(49),
+        50 => Some(50),
+        51 => Some(51),
+        52 => Some(52),
+        53 => Some(53),
+        54 => Some(54),
+        55 => Some(55),
+        56 => Some(56),
+        57 => Some(57),
+        115 => Some(115),
+        99 => Some(99),
+        97 => Some(97),
+        108 => Some(108),
+        101 => Some(101),
+        106 => Some(106),
+        123 => Some(123),
+        125 => Some(125),
+        43 => Some(43),
+        45 => Some(45),
+        42 => Some(42),
+        47 => Some(47),
+        10 => Some(10), // \n
+        27 => Some(27), // escape key
+        127 => Some(127), // delete key
         _ => None,
     }
 }

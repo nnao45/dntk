e952cd5d70265d8a5bcef6edb814f9a106975221
nnao45 <n4sekai5y@gmail.com>
2019-05-25 16:48:36 +0900

    impl fin

--- src/dntker/mod.rs
+++ src/dntker/mod.rs
@@ -425,6 +425,8 @@ mod dntker_tests {
     #[test]
     fn test_dntk_exec() {
         let d1 = &mut Dntker::new();
+        let ptr_escape: [libc::c_char; 1] = [util::ASCII_CODE_ESCAPE as i8; 1];
+        assert_eq!(DntkResult::Fin, d1.dntk_exec(ptr_escape));
         let ptr1: [libc::c_char; 1] = [util::ASCII_CODE_ONE as i8; 1];
         assert_eq!(DntkResult::Output("\u{1b}[36m\r(dntk): 1 = 1\u{1b}[0m\u{1b}[4D".to_string()), d1.dntk_exec(ptr1));
         let ptr2: [libc::c_char; 1] = [util::ASCII_CODE_PLUS as i8; 1];
@@ -437,5 +439,7 @@ mod dntker_tests {
         assert_eq!(DntkResult::Output("\u{1b}[36m\r(dntk): 1+0. = 1\u{1b}[0m\u{1b}[4D".to_string()), d1.dntk_exec(ptr_unknown_ascii));
         let ptr5: [libc::c_char; 1] = [util::ASCII_CODE_SEVEN as i8; 1];
         assert_eq!(DntkResult::Output("\u{1b}[36m\r(dntk): 1+0.7 = 1.7\u{1b}[0m\u{1b}[6D".to_string()), d1.dntk_exec(ptr5));
+        let ptr_enter: [libc::c_char; 1] = [util::ASCII_CODE_NEWLINE as i8; 1];
+        assert_eq!(DntkResult::Fin, d1.dntk_exec(ptr_enter));
     }
 }

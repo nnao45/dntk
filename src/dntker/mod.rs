dc1ca567bf8a2893f27216e493e3c4ea22566942
nnao45 <n4sekai5y@gmail.com>
2019-05-29 09:27:36 +0900

    refactoring

--- src/dntker/mod.rs
+++ src/dntker/mod.rs
@@ -34,6 +34,46 @@ enum DntkResult {
     Continue,
 }
 
+struct DntkString {
+    data: String,
+    dtype: DntkStringType,
+    cur_pos_from_right: usize,
+}
+
+enum DntkStringType {
+    Ok,
+    Ng,
+    Warn,
+}
+
+impl DntkString {
+    pub fn colorize(mut self) -> Self {
+        match &self.dtype {
+            DntkStringType::Ok => {
+                self.data = format!("{}{}{}", util::COLOR_CYAN_HEADER, &self.data, util::COLOR_PLAIN_HEADER);
+                self
+            },
+            DntkStringType::Ng => {
+                self.data = format!("{}{}{}", util::COLOR_MAGENDA_HEADER, &self.data, util::COLOR_PLAIN_HEADER);
+                self
+            },
+            DntkStringType::Warn => {
+                self.data = format!("{}{}{}", util::COLOR_YELLOW_HEADER, &self.data, util::COLOR_PLAIN_HEADER);
+                self
+            },
+        }
+    }
+
+    pub fn cursorize(mut self) -> Self {
+        self.data = format!("{}{}{}{}", &self.data, util::CURSOR_MOVE_ES_HEAD, &self.cur_pos_from_right, util::CURSOR_MOVE_ES_BACK);
+        self
+    }
+
+    pub fn to_string(self) -> String {
+        format!("{}", &self.data)
+    }
+}
+
 impl Dntker {
     pub fn new() -> Self {
         Dntker {
@@ -122,38 +162,41 @@ impl Dntker {
         format!("\r{}", (0..len).map(|_| " ").collect::<String>())
     }
 
-    fn output(&self, v: &mut Vec<&str>, p1: usize, p2: usize) -> String {
-        if meta::build_cli().get_matches().is_present("white") {
-            v.remove(p1);
-            v.remove(p2);
-        }
-        v.iter().map(|s| s.to_string()).collect()
-    }
-
-    fn output_ok(&mut self, p1: &str, p2: &str, p3: &str, p4: &str) -> String {
+    fn output_ok(&mut self, p1: &str, p2: &str, p3: &str, p4: &str) -> DntkString {
         self.before_printed_result_len = p4.to_string().len();
         self.before_printed_statement_len = p2.to_string().len();
         self.before_printed_len = p1.to_string().len() + self.before_printed_statement_len + p3.to_string().len() + self.before_printed_result_len;
         let pos_differnce = self.before_printed_statement_len - self.currnet_cur_pos;
-        let pos_move_point = (p3.to_string().len() + self.before_printed_result_len + &pos_differnce).to_string();
-        let result = &mut vec![util::COLOR_CYAN_HEADER, p1, p2, p3, p4, util::COLOR_PLAIN_HEADER, util::CURSOR_MOVE_ES_HEAD, &pos_move_point, util::CURSOR_MOVE_ES_BACK];
-        self.output(result, 0, 4)
+        DntkString {
+            data: format!("{}{}{}{}", p1, p2, p3, p4),
+            dtype: DntkStringType::Ok,
+            cur_pos_from_right: (p3.to_string().len() + self.before_printed_result_len + &pos_differnce),
+        }
     }
 
-    fn output_ng(&mut self, p1: &str, p2: &str, p3: &str) -> String {
+    fn output_ng(&mut self, p1: &str, p2: &str, p3: &str) -> DntkString {
         self.before_printed_statement_len = p2.to_string().len();
         self.before_printed_len = p1.to_string().len() +  self.before_printed_statement_len + p3.to_string().len() + self.before_printed_result_len;
         let pos_differnce =  self.before_printed_statement_len - &self.currnet_cur_pos;
-        let pos_move_point = (p3.to_string().len() + pos_differnce).to_string();
-        let result = &mut vec![util::COLOR_MAGENDA_HEADER, p1, p2, p3, util::COLOR_PLAIN_HEADER, util::CURSOR_MOVE_ES_HEAD, &pos_move_point, util::CURSOR_MOVE_ES_BACK];
-        self.output(result, 0, 3)
+        DntkString {
+            data: format!("{}{}{}", p1, p2, p3),
+            dtype: DntkStringType::Ng,
+            cur_pos_from_right: (p3.to_string().len() + &pos_differnce),
+        }
     }
 
     fn info_wn(&mut self, unknown_code: &u8) {
         print!("{}", self.output_fill_whitespace(self.before_printed_len));
         let warn_str = format!("this char is no supported: {}", unknown_code.to_owned() as char);
-        let result = &mut vec!["\r", util::COLOR_YELLOW_HEADER, &warn_str, util::COLOR_PLAIN_HEADER];
-        print!("{}", self.output(result, 1, 2));
+        print!("{}", DntkString {
+            data: format!("{}{}", "\r", &warn_str),
+            dtype: DntkStringType::Warn,
+            cur_pos_from_right: 0,
+        }
+            .colorize()
+            .cursorize()
+            .to_string()
+        );
         std::io::stdout().flush().unwrap();
         std::thread::sleep(std::time::Duration::from_millis(1000));
         print!("{}", &self.output_fill_whitespace(warn_str.len()));
@@ -217,17 +260,31 @@ impl Dntker {
         let p3 = " = ";
         match &self.executer.exec(p2) {
             Ok(p4) => {
-                DntkResult::Output(self.output_ok(&p1, p2, p3, &p4))
+                DntkResult::Output(self.output_ok(&p1, p2, p3, &p4)
+                                     .colorize()
+                                     .cursorize()
+                                     .to_string())
             },
             Err(e) => {
                 match e {
                     bc::BcError::PopenError(e) => panic!("call bc process open error: {:?}", e),
                     bc::BcError::Timeout => panic!("call bc process is timeout"),
                     _ => {
-                        DntkResult::Output(self.output_ng(&p1, p2, p3))
+                        DntkResult::Output(self.output_ng(&p1, p2, p3)
+                                     .colorize()
+                                     .cursorize()
+                                     .to_string())
                     },
                 }
             },
+        }
+    }
+
+    pub fn watch(&self,  ptr: [libc::c_char; 3]) -> [libc::c_char; 3] {
+        loop{
+            if unsafe { libc::read(0, ptr.as_ptr() as *mut libc::c_void, 3) } > 0 {
+                return ptr
+            };
         }
     }
 
@@ -242,21 +299,19 @@ impl Dntker {
         let ptr: [libc::c_char; 3] = [0; 3];
 
         print!("{}", util::DNTK_PROMPT);
+        std::io::stdout().flush().unwrap();
         loop {
-            let r = unsafe { libc::read(0, ptr.as_ptr() as *mut libc::c_void, 3) };
-            if r > 0 {
-                match self.dntk_exec(ptr) {
-                    DntkResult::Fin => {
-                        print!("\n");
-                        break
-                    },
-                    DntkResult::Continue => {
-                        continue
-                    },
-                    DntkResult::Output(o) => {
-                        print!("{}", o);
-                    },
-                }
+            match self.dntk_exec(self.watch(ptr)) {
+                DntkResult::Fin => {
+                    print!("\n");
+                    break
+                },
+                DntkResult::Continue => {
+                    continue
+                },
+                DntkResult::Output(o) => {
+                    print!("{}", o);
+                },
             }
             std::io::stdout().flush().unwrap();
         }
@@ -490,15 +545,15 @@ mod dntker_tests {
     #[test]
     fn test_output_ok() {
         let d = &mut Dntker::new();
-        assert_eq!("\u{1b}[36m\r(dntk): 1+2 = 3\u{1b}[0m\u{1b}[7D".to_string(), d.output_ok(util::DNTK_PROMPT, "1+2", " = ", "3"));
-        assert_eq!("\u{1b}[36m\r(dntk): a(123) = 1.56266642461495270762\u{1b}[0m\u{1b}[31D".to_string(), d.output_ok(util::DNTK_PROMPT, "a(123)", " = ", "1.56266642461495270762"));
+        assert_eq!("\u{1b}[36m\r(dntk): 1+2 = 3\u{1b}[0m\u{1b}[7D".to_string(), d.output_ok(util::DNTK_PROMPT, "1+2", " = ", "3").data);
+        assert_eq!("\u{1b}[36m\r(dntk): a(123) = 1.56266642461495270762\u{1b}[0m\u{1b}[31D".to_string(), d.output_ok(util::DNTK_PROMPT, "a(123)", " = ", "1.56266642461495270762").data);
     }
 
     #[test]
     fn test_output_ng() {
         let d = &mut Dntker::new();
-        assert_eq!("\u{1b}[35m\r(dntk): 1+2* = \u{1b}[0m\u{1b}[7D".to_string(), d.output_ng(util::DNTK_PROMPT, "1+2*", " = "));
-        assert_eq!("\u{1b}[35m\r(dntk): a(123)*s( = \u{1b}[0m\u{1b}[12D".to_string(), d.output_ng(util::DNTK_PROMPT, "a(123)*s(", " = "));
+        assert_eq!("\u{1b}[35m\r(dntk): 1+2* = \u{1b}[0m\u{1b}[7D".to_string(), d.output_ng(util::DNTK_PROMPT, "1+2*", " = ").data);
+        assert_eq!("\u{1b}[35m\r(dntk): a(123)*s( = \u{1b}[0m\u{1b}[12D".to_string(), d.output_ng(util::DNTK_PROMPT, "a(123)*s(", " = ").data);
     }
 
     #[test]

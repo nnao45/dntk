94a8796f2050e68b197a79aa166aad4746ed5b95
nnao45 <n4sekai5y@gmail.com>
2019-05-30 10:42:37 +0900

    fix

--- src/main.rs
+++ src/main.rs
@@ -16,14 +16,13 @@ fn main() {
     let _matches = meta::build_cli().get_matches();
 
     #[cfg(not(target_os = "windows"))]
-    {
-        let saved_termattr = term::get_termattr();
-        defer!(
-            unsafe {
-                libc::tcsetattr(0, libc::TCSANOW, &saved_termattr);
-            }
-        );
-    }
+    let saved_termattr = term::get_termattr();
+    #[cfg(not(target_os = "windows"))]
+    defer!(
+        unsafe {
+            libc::tcsetattr(0, libc::TCSANOW, &saved_termattr);
+        }
+    );
 
     #[cfg(target_os = "windows")]
     ansi_term::enable_ansi_support().unwrap();

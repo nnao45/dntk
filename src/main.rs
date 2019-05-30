74d03e72222936ee4c53289e1dec7449292367a2
nnao45 <n4sekai5y@gmail.com>
2019-05-30 10:12:05 +0900

    fix

--- src/main.rs
+++ src/main.rs
@@ -16,13 +16,14 @@ fn main() {
     let _matches = meta::build_cli().get_matches();
 
     #[cfg(not(target_os = "windows"))]
-    let saved_termattr = term::get_termattr();
-    defer!(
-        unsafe {
-            #[cfg(not(target_os = "windows"))]
-            libc::tcsetattr(0, libc::TCSANOW, &saved_termattr);
-        }
-    );
+    {
+        let saved_termattr = term::get_termattr();
+        defer!(
+            unsafe {
+                libc::tcsetattr(0, libc::TCSANOW, &saved_termattr);
+            }
+        );
+    }
 
     #[cfg(target_os = "windows")]
     ansi_term::enable_ansi_support().unwrap();

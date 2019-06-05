22fd012d70c2047baab8b54eecbdc36b4610d128
nnao45 <n4sekai5y@gmail.com>
2019-06-05 12:02:49 +0900

    little fix

--- src/main.rs
+++ src/main.rs
@@ -18,14 +18,13 @@ mod dntker;
 
 fn main() {
     #[cfg(not(target_os = "windows"))]
-    {
-        let saved_termattr = term::get_termattr();
-        defer!(
-            unsafe {
-                libc::tcsetattr(0, libc::TCSANOW, &saved_termattr);
-            }
-        );
-    }
+    let saved_termattr = term::get_termattr();
+    #[cfg(not(target_os = "windows"))]
+    defer!(
+        unsafe {
+            libc::tcsetattr(0, libc::TCSANOW, &saved_termattr);
+        }
+    );
 
     #[cfg(target_os = "windows")] 
     term::enable_ansi().unwrap();

a1ebae21dabb6a5fea51f2a840bdd7e9fae8675e
nnao45 <n4sekai5y@gmail.com>
2019-06-06 10:55:54 +0900

    fifix

--- ci/script.sh
+++ ci/script.sh
@@ -7,8 +7,6 @@ cross build --target $TARGET
 if [[ "${TARGET}" = "i686-unknown-freebsd" ]] || [[ "${TARGET}" = "x86_64-unknown-freebsd" ]]; then
     echo "'cross test' command is not available for '${TARGET}' target"
     cargo install --force cross
-    cross build --target $TARGET
 else
     cargo test  --target $TARGET
-    cargo build --target $TARGET
 fi

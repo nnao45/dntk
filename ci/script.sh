5c141dffab0a13e198cde8e4d02c5c51c903a88f
nnao45 <n4sekai5y@gmail.com>
2019-06-06 12:09:12 +0900

    test ignore

--- ci/script.sh
+++ ci/script.sh
@@ -4,6 +4,8 @@ export PKG_CONFIG_ALLOW_CROSS=1
 
 if [[ "${TARGET}" = "i686-unknown-freebsd" ]] || [[ "${TARGET}" = "x86_64-unknown-freebsd" ]]; then
     cargo install --force cross
+elif [[ "${TARGET}" = "aarch64-unknown-linux-gnu" ]]; then
+    echo 'cargo test not support'
 else
     cargo test  --target $TARGET
 fi

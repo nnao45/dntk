14928e9acfdc7413e8fc10a1dc8f326d9905afe6
nnao45 <n4sekai5y@gmail.com>
2019-06-06 11:01:18 +0900

    typ

--- ci/script.sh
+++ ci/script.sh
@@ -3,7 +3,6 @@ set -ex
 
 export PKG_CONFIG_ALLOW_CROSS=1
 
-cross build --target $TARGET
 if [[ "${TARGET}" = "i686-unknown-freebsd" ]] || [[ "${TARGET}" = "x86_64-unknown-freebsd" ]]; then
     echo "'cross test' command is not available for '${TARGET}' target"
     cargo install --force cross

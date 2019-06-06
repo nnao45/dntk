ef076608e30f919233f01af5d9a31bafdefaf5b7
nnao45 <n4sekai5y@gmail.com>
2019-06-06 14:30:30 +0900

    typo

--- ci/before_deploy.sh
+++ ci/before_deploy.sh
@@ -11,7 +11,7 @@ fi
 mkdir "dntk-${TRAVIS_TAG}-${TARGET}"
 cp target/$TARGET/release/dntk LICENSE README.md "dntk-${TRAVIS_TAG}-${TARGET}"
 
-if [[ "${TARGET}" == "i686-pc-windows-msvc" ]] || [[ "${TARGET}" == "x86_64-pc-windows-msvc" ]] ; then
+if [[ "${TARGET}" == "i686-pc-windows-msvc" ]] || [[ "${TARGET}" == "x86_64-pc-windows-msvc" ]]; then
     7z.exe a "dntk-${TRAVIS_TAG}-${TARGET}.zip" "dntk-${TRAVIS_TAG}-${TARGET}"
 else
     zip "dntk-${TRAVIS_TAG}-${TARGET}.zip" -r "dntk-${TRAVIS_TAG}-${TARGET}"

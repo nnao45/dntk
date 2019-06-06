de592cf7944a2ca3b78e4e8ef55c97b6e9638b92
nnao45 <n4sekai5y@gmail.com>
2019-06-06 11:15:41 +0900

    fix

--- ci/script.sh
+++ ci/script.sh
@@ -1,10 +1,8 @@
 #!/bin/bash
 set -ex
-
 export PKG_CONFIG_ALLOW_CROSS=1
 
 if [[ "${TARGET}" = "i686-unknown-freebsd" ]] || [[ "${TARGET}" = "x86_64-unknown-freebsd" ]]; then
-    echo "'cross test' command is not available for '${TARGET}' target"
     cargo install --force cross
 else
     cargo test  --target $TARGET
--- src/dntker/tests.rs
+++ src/dntker/tests.rs
@@ -303,7 +303,31 @@ mod dntker_tests {
     }
 
     #[test]
-    #[cfg(not(target_os = "windows"))]
+    #[cfg(target_arch = "aarch64")]
+    fn test_dntk_exec() {
+        let d1 = &mut Dntker::new();
+        let ptr_escape: [libc::c_char; 3] = [util::ASCII_CODE_ESCAPE as u8; 3];
+        assert_eq!(DntkResult::Fin, d1.dntk_exec(ptr_escape));
+        let ptr1: [libc::c_char; 3] = [util::ASCII_CODE_ONE as u8; 3];
+        assert_eq!(DntkResult::Output("\u{1b}[36m\r(dntk): 1 = 1\u{1b}[0m".to_string()), d1.dntk_exec(ptr1));
+        let ptr_right: [libc::c_char; 3] = [util::ASCII_CODE_ESCAPE as u8, 0x91 as u8, util::ASCII_CODE_RIGHT as u8];
+        assert_eq!(DntkResult::Output("\u{1b}[36m\r(dntk): 1 = 1\u{1b}[0m".to_string()), d1.dntk_exec(ptr_right));
+        let ptr2: [libc::c_char; 3] = [util::ASCII_CODE_PLUS as u8; 3];
+        assert_eq!(DntkResult::Output("\u{1b}[35m\r(dntk): 1+ = \u{1b}[0m".to_string()), d1.dntk_exec(ptr2));
+        let ptr3: [libc::c_char; 3] = [util::ASCII_CODE_ZERO as u8; 3];
+        assert_eq!(DntkResult::Output("\u{1b}[36m\r(dntk): 1+0 = 1\u{1b}[0m".to_string()), d1.dntk_exec(ptr3));
+        let ptr4: [libc::c_char; 3] = [util::ASCII_CODE_DOT as u8; 3];
+        assert_eq!(DntkResult::Output("\u{1b}[36m\r(dntk): 1+0. = 1\u{1b}[0m".to_string()), d1.dntk_exec(ptr4));
+        let ptr_unknown_ascii: [libc::c_char; 3] = [0x4f as u8; 3];
+        assert_eq!(DntkResult::Output("\u{1b}[36m\r(dntk): 1+0. = 1\u{1b}[0m".to_string()), d1.dntk_exec(ptr_unknown_ascii));
+        let ptr5: [libc::c_char; 3] = [util::ASCII_CODE_SEVEN as u8; 3];
+        assert_eq!(DntkResult::Output("\u{1b}[36m\r(dntk): 1+0.7 = 1.7\u{1b}[0m".to_string()), d1.dntk_exec(ptr5));
+        let ptr_enter: [libc::c_char; 3] = [util::ASCII_CODE_NEWLINE as u8; 3];
+        assert_eq!(DntkResult::Fin, d1.dntk_exec(ptr_enter));
+    }
+
+    #[test]
+    #[cfg(any(not(target_os = "windows"), not(target_arch = "aarch64")))]
     fn test_dntk_exec() {
         let d1 = &mut Dntker::new();
         let ptr_escape: [libc::c_char; 3] = [util::ASCII_CODE_ESCAPE as i8; 3];

c398d6db643515794c98e71cfa16a48c0340e95b
nnao45 <n4sekai5y@gmail.com>
2019-12-16 23:54:18 +0900

    もう１回

--- ci/script.sh
+++ ci/script.sh
@@ -4,7 +4,7 @@ export PKG_CONFIG_ALLOW_CROSS=1
 
 if [[ "${TARGET}" = "i686-unknown-freebsd" ]] || [[ "${TARGET}" = "x86_64-unknown-freebsd" ]]; then
     cargo install --force cross
-    cross clippy --target=${TARGET} -- -D warnings
+    cross clippy --target ${TARGET} -- -D warnings
     cross build --target ${TARGET} --release
 else
     cargo test  --target ${TARGET}

f7a5d94567c7666f38040e8d2f371eb34ea56081
nnao45 <n4sekai5y@gmail.com>
2025-10-19 00:50:57 +0900

    release v3.0.12

--- release.sh
+++ release.sh
@@ -8,4 +8,5 @@ if ! printf ${ANSWER} | egrep ${SEMATIC_VERSION_REGEX} >/dev/null 2>&1; then
 fi
 echo "release ${ANSWER}"
 sed -i -e "s/version = \"$(./version.sh | cut -c 2-)\"/version = \"${ANSWER}\"/g" Cargo.toml
-sleep 10
+cargo update -p dntk
+echo "Cargo.toml and Cargo.lock updated to version ${ANSWER}"
